0519d8cdd8b1be308bf6c5623080940b20615b5b
Алина <shcherbinina@users.noreply.github.com>
2020-11-03 09:53:14 +0800

    Fixed events and routing; events coud be seen in logs/laravel.log

--- app/Events/SendTweet.php
+++ app/Events/SendTweet.php
@@ -27,13 +27,4 @@ class SendTweet
         $this -> user = $user; 
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
 }
--- app/Listeners/NotificationSendTweet.php
+++ app/Listeners/NotificationSendTweet.php
@@ -5,8 +5,7 @@ namespace App\Listeners;
 use App\Events\SendTweet;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class NotificationSendTweet
 {
@@ -30,10 +29,10 @@ class NotificationSendTweet
     {
         $data = array('name' => $event -> user-> name,
         'email' => $event -> user -> email,
-        'body' => 'Thank ypu for joining Twittor! i jope you will enjoy the loneliness!!');
+        'body' => 'Thank you for joining Twittor! i hope you will enjoy the loneliness!!');
 
 
-        Mail::send('email', $data, function($message) use ($data) {
+        Log::channel('stack')->info('email', $data, function($message) use ($data) {
             $message -> to($data['email'])->subject('Thank you letter');
             $message -> from('noreply@artisanweb.net');
         });
